ba50dcb84cb7558a27ae509ca43ea001b2596ae4
poulomee13 <66532775+poulomee13@users.noreply.github.com>
2020-07-28 12:52:19 +0530

    Update SQLTasks Tier 1.sql

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -34,19 +34,19 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
----SELECT name FROM `Facilities` WHERE membercost > 0
+SELECT name FROM `Facilities` WHERE membercost > 0
 
 /* Q2: How many facilities do not charge a fee to members? */
 
 
----SELECT count(*) FROM `Facilities` WHERE membercost = 0
+SELECT count(*) FROM `Facilities` WHERE membercost = 0
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
----SELECT facid, name, membercost, monthlymaintenance 
+   SELECT facid, name, membercost, monthlymaintenance 
    FROM `Facilities` 
    where membercost < (monthlymaintenance * .20)
 
@@ -54,7 +54,7 @@ facilities in question. */
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
----select * from `Facilities` where facid IN (1,5)
+select * from `Facilities` where facid IN (1,5)
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -63,7 +63,7 @@ more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
 
----SELECT name,monthlymaintenance,
+SELECT name,monthlymaintenance,
 Case when monthlymaintenance <= 100 then 'cheap'
      else 'expensive' 
 END as costtype
@@ -72,14 +72,14 @@ FROM `Facilities`
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
----SELECT firstname, surname FROM `Members` where joindate = (select max(joindate) from `Members`)
+SELECT firstname, surname FROM `Members` where joindate = (select max(joindate) from `Members`)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
----SELECT distinct concat(f.name ,'-', m.firstname,m.surname) as courtmember
+SELECT distinct concat(f.name ,'-', m.firstname,m.surname) as courtmember
 FROM `Bookings` b join `Facilities` f 
 ON b.facid = f.facid 
 join `Members` m on b.memid = m.memid
@@ -93,7 +93,7 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
----select f.name as faciliy, concat(m.firstname,m.surname) as name,f.membercost,f.guestcost  
+Select f.name as faciliy, concat(m.firstname,m.surname) as name,f.membercost,f.guestcost  
 from `Bookings` b join `Facilities` f 
 ON b.facid = f.facid 
 join `Members` m on b.memid = m.memid
@@ -104,7 +104,7 @@ order by f.membercost,f.guestcost desc
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
----select a.facility, a.name, a.membercost, a.guestcost from
+Select a.facility, a.name, a.membercost, a.guestcost from
 (select f.name as facility, concat(m.firstname,m.surname) as name,sum(f.membercost) as membercost,sum(f.guestcost) as guestcost
 from `Bookings` b join `Facilities` f 
 ON b.facid = f.facid 
@@ -134,7 +134,7 @@ QUESTIONS:
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
 
----select a.name,a.revenue from 
+Select a.name,a.revenue from 
 (select name, (membercost+guestcost) as revenue
 from `Facilities`
 group by name)a
@@ -143,18 +143,13 @@ order by revenue
 
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
----SELECT a.memid, a.surname, a.firstname,(select CASE when k.firstname != 'GUEST' then CONCAT(k.firstname,' ',k.surname) else k.firstname end from `Members` k where a.recommendedby = k.memid) AS recommendedby
+SELECT a.memid, a.surname, a.firstname,(select CASE when k.firstname != 'GUEST' then CONCAT(k.firstname,' ',k.surname) else k.firstname end from `Members` k where a.recommendedby = k.memid) AS recommendedby
 FROM `Members` a 
 ORDER BY a.surname, a.firstname
 
 
 /* Q12: Find the facilities with their usage by member, but not guests */
----SELECT f.name, count(b.memid) as members
-FROM `Facilities` f JOIN `Bookings` b ON f.facid = b.facid 
-where b.memid != 0
-group by f.name
 
---2---
 SELECT f.name,concat(m.firstname,' ',m.surname) as member, count(b.bookid) as usuage
 FROM `Facilities` f JOIN `Bookings` b ON f.facid = b.facid 
 JOIN `Members` m ON b.memid = m.memid
@@ -165,12 +160,7 @@ group by f.name,concat(m.firstname,' ',m.surname)
 
 
 /* Q13: Find the facilities usage by month, but not guests */
----SELECT f.name, count(MONTH(b.starttime)) as months
-FROM `Facilities` f JOIN `Bookings` b ON f.facid = b.facid 
-where b.memid != 0
-group by f.name
 
----2 ---
 SELECT f.name, DATE_FORMAT(b.starttime, '%M-%y') as Month,count(b.bookid) as Bookings
 FROM `Facilities` f JOIN `Bookings` b ON f.facid = b.facid 
 where b.memid != 0
